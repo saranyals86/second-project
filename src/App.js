e294cd5198f87753c5c41e14e934df0e1fd61065
Saranya <saranyals@gmail.com>
2023-02-08 22:23:24 -0500

    updated a comment in App js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Happy for my progress.
+          Demo project in our workshop.
         </p>
         <a
           className="App-link"
@@ -18,7 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>  
   );
 }
 
